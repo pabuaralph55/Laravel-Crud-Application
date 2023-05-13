2d5f8f466cd6437658c33fb9f534789dbbf74aaa
ralph Samonte pabua <87641822+pabuaralph55@users.noreply.github.com>
2023-05-14 07:23:53 +0800

    Last update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,10 +10,6 @@
     <link rel="stylesheet" href="{{ asset('css/create.css') }}">
     <link rel="stylesheet" href="{{ asset('css/edit.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-    
-    
-
-    
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     
     @yield('styles')
